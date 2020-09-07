89b726420c8c0b36248ed86cce1605e4d5c37e85
keifer <kjoedicker@gmail.com>
2020-09-07 13:51:09 -0400

    remove test1 test2 to understand commits

--- src/source/program_actions.c
+++ src/source/program_actions.c
@@ -22,9 +22,6 @@ void DirectFlow(int argc, char *argv[])
             fprintf(stderr, "%s missing argument", argv[1]);
             break;
 
-            test1
-            test2
-
         default:
             ParseFlags(argc, argv);
             break;
