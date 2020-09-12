e2930d453fc54837e00dd4395e9af05af9ab293c
keifer <kjoedicker@gmail.com>
2020-09-12 13:34:16 -0400

    fixed ListDir to account for changing .trash folder structure

--- src/source/file_manipulation.c
+++ src/source/file_manipulation.c
@@ -20,7 +20,7 @@ void DeleteFile(struct Logistics *core_logistics, struct Argument *target_file)
 void ListDir(char *target_folder, int size_details) 
 { 
     struct stat file_stat;
-    
+
     //readdir() will access directory_stream returning a pointer to a entry to be accessed
     struct dirent *directory_entry;  
 
@@ -29,45 +29,41 @@ void ListDir(char *target_folder, int size_details)
     
     if (directory_stream)
     {
-        printf("here");
         //access the next dirent in the directory_stream, filtering out '.' ".." path directives
         while (((directory_entry = readdir(directory_stream)) != NULL) &&
                 (!(cmpstr(directory_entry->d_name, "."))) &&
                 (!(cmpstr(directory_entry->d_name, ".."))) )
         {      
-            //because files in .trash are nested in a self named folder we need to append for that depth for the full pwd     
-            size_t filename_size = (strlen(target_folder) + (strlen(directory_entry->d_name) * 2)) + 2;
+            // //because files in .trash are nested we need to append for that depth for the full pwd     
+            size_t filename_size = (strlen(target_folder) + strlen(directory_entry->d_name)) + 2;
             char *file_path = concat(filename_size,
-                                       concat(filename_size - strlen(directory_entry->d_name), 
-                                                target_folder,
-                                                SEPARATOR,                                                                                            
-                                                directory_entry->d_name,
-                                                KEEP_HEAD),
-                                       SEPARATOR,
-                                       directory_entry->d_name,
-                                       FREE_HEAD);
+                                     target_folder,            
+                                     SEPARATOR,
+                                     directory_entry->d_name,
+                                     KEEP_HEAD);
+
             
             if (access(file_path, F_OK) + 1)
             {
-                printf("here");
                 switch (size_details)
                 {
                     case VERBOSE:
-                        stat(file_path, &file_stat);
 
+                        stat(file_path, &file_stat);
+                                                
                         printf("%-25s %6u bytes\n", directory_entry->d_name, (unsigned int)file_stat.st_size);
+
                         break;
                     
                     case CONCISE:
-                        printf("%s ", directory_entry->d_name);
+                        printf("%s\n", directory_entry->d_name);
                         break;
                 }
+
+                free(file_path);
             }
-
-            free(file_path);   
         }
-
-        closedir(directory_stream);     
+        closedir(directory_stream);   
     }
 
     else 
