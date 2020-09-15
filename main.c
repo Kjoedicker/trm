03efd3f9477daa56c96b5b131503f2e2c4ae7015
keifer <kjoedicker@gmail.com>
2020-09-15 12:04:05 -0400

    Cleaned up main.c

--- main.c
+++ main.c
@@ -3,52 +3,8 @@
      
 int main(int argc, char *argv[], char **envp)
 {
-    // RestoreFile(InitLogistics(), argv[1], 1);
     ParseFlags(argc, argv);
-    // rename("/home/keifer/.trash/1", "/home/keifer/1");
-    // struct Logistics *core_logistics = InitLogistics();
-
-
-    // DeleteFile(core_logistics, ParseFile(argv[1]));
-
-    // struct Argument *zero = ParseFile(argv[1]);
-
-    // struct Argument **arg = malloc(sizeof(struct Argument) * 2);
-    // arg[0] = zero;
-    // printf("%s", arg[0]->parsed_argument);
-
-    // ParseQueuedFiles("~/.trash/", argv, argc-1);
-
-
-
-
-
-
-
-
-    // char test = argv[1][0];
-    // printf("%c", test);
-
-    // ParseFlags(argc, argv);
-    // struct Logistics *tmp = InitLogistics();
-
-    // InitTrashFolder(tmp);
-
-    // DeleteFile(tmp, "test1");
-
-    // ViewTrash(tmp, CONCISE);
-
-    // // char *test[] = {"test1", "test2", "test3", "test4", "test5"};  
-
-    // // for (int i = 0; i < 5; i++)
-    // // {
-    // //     DeleteFile(tmp, test[i]);
-    // //     // printf("%s", test[i]);
-    // // }   
-   
-   
-    // free(tmp);
-
+ 
     return 0;
 }
 
