247ac742c5a3fd1336b50b1384c9e8a2ee8ab9e9
keifer <kjoedicker@gmail.com>
2020-09-18 05:16:24 -0400

    decided to keep struct Logistics as core parameter for listdir because a struct Logisitcs member inti is all that is needed

--- src/header/filelib.h
+++ src/header/filelib.h
@@ -10,8 +10,6 @@
 void deletefile(struct Argument *target_file);
 void restorefile(struct Argument *target_file);
 
-//view the contents of directory
-//(#5 edit to take Argument member)
 void listdir(struct Logistics *core_logistics, int size_details);
 
 enum {CONCISE, VERBOSE};
