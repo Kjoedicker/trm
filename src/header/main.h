802e8bc6e13e439c8805cf78088098f98813b8ae
keifer <kjoedicker@gmail.com>
2020-09-18 15:33:28 -0400

    refactored

--- src/header/main.h
+++ src/header/main.h
@@ -2,33 +2,30 @@
 #ifndef MAIN
 #define MAIN
 
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
 
 
-//actions to be performed
-#include "filelib.h"
-
-//controls the execution of the program based on the flags provided
-#include "flowcontrol.h"
-
-//for initializing essential folders during runtime. (.trash, .trace, .<deleted_folder>, etc.)
-#include "initlib.h"
-
-//implements the logistics struct which contains core information about the directory structures
+//implements the logisitcs struct which contains core information about the directory structures
 #include "logistics.h"
 
 //for all parsing purposes
 #include "parselib.h"
 
+//actions to be performed
+#include "filelib.h"
+
 //supplemental tools to help with logic and overall execution of various functions
 #include "toollib.h"
 
+//for initializing essential folders during runtime. (.trash, .trace, .<deleted_folder>, etc.)
+#include "initlib.h"
 
-
+//controls the execution of the program based on the flags provided
+#include "flowcontrol.h"
 
 
 #endif
