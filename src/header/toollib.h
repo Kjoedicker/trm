f08d5edb910af560e009369d20dee587d7a2def1
keifer <kjoedicker@gmail.com>
2020-09-21 14:33:45 -0400

    Remove TODO(#1)

--- src/header/toollib.h
+++ src/header/toollib.h
@@ -10,7 +10,6 @@ int checkifpath(char *file_path);
 void createfolder(char *folder);
 void writetofile(char *location,  char *message);
 void reextend(struct Argument *target_file);
-
 char *readfile(char *file_path);
 char *extendpath(size_t allocation_size, char *head_pwd, char *separator, char *tail_pwd, int head_direction);
 enum {KEEP_HEAD, FREE_HEAD};
