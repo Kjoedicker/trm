a390e38f7e4493917eac917e20529b4a4458ab80
keifer <kjoedicker@gmail.com>
2020-09-22 15:55:42 -0400

    formatting

--- src/header/main.h
+++ src/header/main.h
@@ -4,10 +4,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-// #include <unistd.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
-
 
 //implements the logistics struct which contains core information about the directory structures
 #include "logistics.h"
@@ -26,6 +25,5 @@
 
 //controls the execution of the program based on the flags provided
 #include "flowcontrol.h"
-
 
 #endif
