f2afd5a68fa1c705166498f23db7af8ad91cd585
keifer <kjoedicker@gmail.com>
2020-09-22 14:53:31 -0400

    Add TODO(#10)

--- src/header/toollib.h
+++ src/header/toollib.h
@@ -9,7 +9,7 @@ int pathexists(char *path);
 int checkifpath(char *file_path);
 void createfolder(char *folder);
 void writetofile(char *location,  char *message);
-// TODO: test
+// TODO(#10): test
 void reextend(struct Argument *target_file);
 char *readfile(char *file_path);
 char *extendpath(size_t allocation_size, char *head_pwd, char *separator, char *tail_pwd, int head_direction);
